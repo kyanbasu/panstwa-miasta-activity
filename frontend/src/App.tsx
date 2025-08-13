3dc4b15b881b8eb08f49653b889d0379dc0466e6
bnm <bartekbata1302@gmail.com>
2025-08-14 00:23:56 +0200

    example lobby name with useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React, { type JSX, useEffect, useState } from "react";
 
 export default function App(): JSX.Element {
+  const [lobbyName, setLobbyName] = useState("unknown");
+
   return (
     <div className="app-root">
-      <header className="topbar">Państwa miasta</header>
-      <h1>hello world</h1>
+      <h1 className="inline">Lobby: </h1>
+      <h1 className="inline">{lobbyName}</h1>
+      <h1></h1>
+
+      <h2>hello</h2>
     </div>
   );
 }
