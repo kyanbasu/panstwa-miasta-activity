6d9ec784df3ea81742f6605af8ab0fa2665204aa
bnm <bartekbata1302@gmail.com>
2025-08-13 15:30:05 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Categories (game) Discord Activity (pl. Państwa Miasta)
+
+A word game where players attempt to list words that fit into particular categories, all starting with the same letter.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { JSX, useEffect, useState } from "react";
+
+export default function App(): JSX.Element {
+  return (
+    <div className="app-root">
+      <header className="topbar">Państwa miasta</header>
+      <h1>hello world</h1>
+    </div>
+  );
+}
